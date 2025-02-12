9a4aa96c62466a91d4b54866a104b3fa489c9317
Þorvaldur Tumi Baldursson <tumi@juni.is>
2025-02-12 11:05:03 +0000

    fix create games

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,35 +1,40 @@
-import type { Game, User } from "./types"
+import type { Game, User } from "./types";
 
-const base_url = "https://foosball.steini-b11.workers.dev"
+const base_url = "https://foosball.steini-b11.workers.dev";
 
 export const getAllUsers = async (): Promise<User[]> => {
-    const data = await fetch(`${base_url}/players/all`).then(async (res) => await res.json())
-    return data as User[]
-}
+  const data = await fetch(`${base_url}/players/all`).then(
+    async (res) => await res.json(),
+  );
+  return data as User[];
+};
 
 export const getAllGames = async (): Promise<Game[]> => {
-    const data = await fetch(`${base_url}/games/all`).then(async (res) => await res.json())
-    return data as Game[]
-}
+  const data = await fetch(`${base_url}/games/all`).then(
+    async (res) => await res.json(),
+  );
+  return data as Game[];
+};
 
 export const createGame = async (
-    red_players: number[],
-    red_score: number,
-    blue_players: number[],
-    blue_score: number,
-    api_key: string
+  red_players: number[],
+  red_score: number,
+  blue_players: number[],
+  blue_score: number,
+  api_key: string,
 ): Promise<void> => {
-    await fetch(`${base_url}/games/create`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authentication": `Bearer ${api_key}`
-        },
-        body: JSON.stringify({
-            redTeamPlayers: red_players,
-            redTeamScore: red_score,
-            blueTeamPlayers: blue_players,
-            blueTeamScore: blue_score
-        })
-    })
-}
+  await fetch(`${base_url}/games/new`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authentication: `Bearer ${api_key}`,
+    },
+    body: JSON.stringify({
+      redTeamPlayers: red_players,
+      redTeamScore: red_score,
+      blueTeamPlayers: blue_players,
+      blueTeamScore: blue_score,
+    }),
+  });
+};
+
